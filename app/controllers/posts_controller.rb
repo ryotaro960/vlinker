0c37ddf3f167226951f4af9568a38e8a8c192c0f
ryotaro960 <kasakasaaroe@yahoo.co.jp>
2024-10-17 12:19:14 +0900

    編集ページの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,14 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+    post_attributes = @post.attributes
+    @post_form = PostForm.new(post_attributes)
+    @post_form.movie_tag_name = "#{@post.movie_tags.first&.movie_tag_name}`/$#{@post.movie_tags.second&.movie_tag_name}`/$#{@post.movie_tags.third&.movie_tag_name}"
+    @post_form.talent_tag_name = "#{@post.talent_tags.first&.talent_tag_name}`/$#{@post.talent_tags.second&.talent_tag_name}`/$#{@post.talent_tags.third&.talent_tag_name}"
+  end
+
   private
   
   def post_form_params
--- app/javascript/post.js
+++ app/javascript/post.js
@@ -1,21 +1,27 @@
+// 投稿ページ
 
 const mainmovie = () => {
   const form = document.getElementById('movie_main_form')
   form.addEventListener('blur', () => {
-    let sliceplace = form.value.indexOf('watch?v=');
+
+    const slicewatch = form.value.indexOf('watch?v=');
+    const sliceshorts = form.value.indexOf('/shorts/');
+    let sliceplace = 0;
+    if(slicewatch > 0) {
+      sliceplace = slicewatch;
+    } else if(sliceshorts > 0) {
+      sliceplace = sliceshorts;
+    }
     if(sliceplace > 0) {
       const movieid = form.value.substr( sliceplace + 8, 11 );
       const movieMainEmbeddedForm  = document.getElementById('movie_main_embedded_form');
       movieMainEmbeddedForm.value = movieid;
       const movieMainThumbnailForm  = document.getElementById('movie_main_thumbnail_form');
       movieMainThumbnailForm.value = 'https://img.youtube.com/vi/' + movieid + '/hqdefault.jpg'
-      }
-    sliceplace = form.value.indexOf('/shorts/');
-    if(sliceplace > 0) {
-      const movieid = form.value.substr( sliceplace + 8, 11 );
-      const movieMainEmbeddedForm  = document.getElementById('movie_main_embedded_form');
-      movieMainEmbeddedForm.value = movieid;
-      }
+    } else {
+      document.getElementById('movie_main_embedded_form').value = '';
+      document.getElementById('movie_main_thumbnail_form').value = '';
+    }
   });
  };
  
@@ -26,18 +32,21 @@ window.addEventListener('turbo:render', mainmovie);
 const leftmovie = () => {
 const form = document.getElementById('movie_left_form')
 form.addEventListener('blur', () => {
-  let sliceplace = form.value.indexOf('watch?v=');
+  const slicewatch = form.value.indexOf('watch?v=');
+  const sliceshorts = form.value.indexOf('/shorts/');
+  let sliceplace = 0;
+  if(slicewatch > 0) {
+    sliceplace = slicewatch;
+  } else if(sliceshorts > 0) {
+    sliceplace = sliceshorts;
+  }
   if(sliceplace > 0) {
     const movieid = form.value.substr( sliceplace + 8, 11 );
-    const movieMainEmbeddedForm  = document.getElementById('movie_left_embedded_form');
-    movieMainEmbeddedForm.value = movieid;
-    }
-  sliceplace = form.value.indexOf('/shorts/');
-  if(sliceplace > 0) {
-    const movieid = form.value.substr( sliceplace + 8, 11 );
-    const movieMainEmbeddedForm  = document.getElementById('movie_left_embedded_form');
-    movieMainEmbeddedForm.value = movieid;
-    }
+    const movieLeftEmbeddedForm  = document.getElementById('movie_left_embedded_form');
+    movieLeftEmbeddedForm.value = movieid;
+  } else {
+    document.getElementById('movie_left_embedded_form').value = '';
+  }
 });
 }
 
@@ -48,18 +57,21 @@ window.addEventListener('turbo:render', leftmovie);
 const rightmovie = () => {
   const form = document.getElementById('movie_right_form')
   form.addEventListener('blur', () => {
-    let sliceplace = form.value.indexOf('watch?v=');
+    const slicewatch = form.value.indexOf('watch?v=');
+    const sliceshorts = form.value.indexOf('/shorts/');
+    let sliceplace = 0;
+    if(slicewatch > 0) {
+      sliceplace = slicewatch;
+    } else if(sliceshorts > 0) {
+      sliceplace = sliceshorts;
+    }
     if(sliceplace > 0) {
       const movieid = form.value.substr( sliceplace + 8, 11 );
-      const movieMainEmbeddedForm  = document.getElementById('movie_right_embedded_form');
-      movieMainEmbeddedForm.value = movieid;
-      }
-    sliceplace = form.value.indexOf('/shorts/');
-    if(sliceplace > 0) {
-      const movieid = form.value.substr( sliceplace + 8, 11 );
-      const movieMainEmbeddedForm  = document.getElementById('movie_right_embedded_form');
-      movieMainEmbeddedForm.value = movieid;
-      }
+      const movieRightEmbeddedForm  = document.getElementById('movie_right_embedded_form');
+      movieRightEmbeddedForm.value = movieid;
+    } else {
+      document.getElementById('movie_right_embedded_form').value = '';
+    }
   });
   }
   
@@ -75,7 +87,7 @@ const rightmovie = () => {
         const movieSecondtag  = document.getElementById('movie_secondtag');
         const movieThirdtag  = document.getElementById('movie_thirdtag');
         const movieMainTagsMerged  = document.getElementById('movie_main_tags_merged');
-        movieMainTagsMerged.value = movieFirsttag.value + '`/#' + movieSecondtag.value + '`/#' + movieThirdtag.value
+        movieMainTagsMerged.value = movieFirsttag.value + '`/$' + movieSecondtag.value + '`/$' + movieThirdtag.value
       });
     });
   };
@@ -92,10 +104,47 @@ const rightmovie = () => {
         const talentSecondtag  = document.getElementById('talent_secondtag');
         const talentThirdtag  = document.getElementById('talent_thirdtag');
         const talentTagsMerged  = document.getElementById('talent_tags_merged');
-        talentTagsMerged.value = talentFirsttag.value + '`/#' + talentSecondtag.value + '`/#' + talentThirdtag.value
+        talentTagsMerged.value = talentFirsttag.value + '`/$' + talentSecondtag.value + '`/$' + talentThirdtag.value
       });
     });
   };
 
   window.addEventListener('turbo:load', talenttag);
   window.addEventListener('turbo:render', talenttag);
+
+  // 詳細ページ
+
+  const menubtn = () => {
+    const pullDownButton = document.getElementById('menu-btn');
+    const pullDownParents = document.getElementById('menu_lists');
+
+    pullDownButton.addEventListener('mouseover', () => {
+    pullDownParents.setAttribute("style", "display:block;");
+
+    pullDownButton.addEventListener('mouseout', () => {
+    pullDownParents.setAttribute("style", "display: none;");
+      })
+    });
+  };
+
+  window.addEventListener('turbo:load', menubtn);
+  window.addEventListener('turbo:render', menubtn);
+
+  // 編集ページ
+
+  const edit_tag = () => {
+
+    const movieTagsMerged  = document.getElementById('movie_main_tags_merged').value;
+    const movie_tag_split = movieTagsMerged.split('`/$');
+    document.getElementById('movie_firsttag').value = movie_tag_split[0];
+    document.getElementById('movie_secondtag').value = movie_tag_split[1];
+    document.getElementById('movie_thirdtag').value = movie_tag_split[2];
+
+    const talentTagsMerged  = document.getElementById('talent_tags_merged').value;
+    const talent_tag_split = talentTagsMerged.split('`/$');
+    document.getElementById('talent_firsttag').value = talent_tag_split[0];
+    document.getElementById('talent_secondtag').value = talent_tag_split[1];
+    document.getElementById('talent_thirdtag').value = talent_tag_split[2];
+  };
+
+  window.addEventListener('turbo:load', edit_tag);

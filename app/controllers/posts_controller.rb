01fc7947bd5f85a3d9c80c6c4513c570569de8d6
Your Name <you@example.com>
2024-10-17 17:44:51 +0900

    一覧ページの調整

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :move_to_index, only: [:edit, :destroy]
 
   def index  
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC")
   end
 
   def new
